cf4a1fa5907b9b010e250f0523f9e2df01fe20e1
Gregory Stula <gregstula.dev@icloud.com>
2016-04-30 12:08:43 -0400

    Update Swift in LiteMatrix

--- source/LiteMatrix.swift
+++ source/LiteMatrix.swift
@@ -10,38 +10,40 @@
 import Foundation
 
 class LiteMatrix<T: NSObject> {
-
-    private let matrix: ObjcLiteMatrix
-    let rowCapacity: Int
-    let colCapacity: Int
     
-    private var initializeTOnMatrixInit: Bool = true
+    private let matrix:ObjcLiteMatrix
+    let rowCapacity:Int
+    let colCapacity:Int
+    
+    private var initializeTOnMatrixInit:Bool = true
     private var warning: String
     private var index = (x:0, y:0)
     
     
     // Designated initializer, initializes a matrix of unique objects
-    init(rows: Int, columns cols: Int) {
+    init(rows:Int, columns cols:Int)
+    {
         rowCapacity = rows
         colCapacity = cols
         
-        matrix = ObjcLiteMatrix(rowSize: rows, withColumnSize: cols)
+        matrix = ObjcLiteMatrix(rowSize:rows, withColumnSize:cols)
         warning = "Index out of range - row capacity: \(rowCapacity), col capacity: \(colCapacity)"
-       
+        
         for i in 0..<rowCapacity {
             for j in 0..<colCapacity {
                 matrix.addObjectToMatrixAtIndex(T() as AnyObject, row:i , column:j)
             }
         }
     }
-
-
+    
+    
     // Initializes a Matrix of references to the same object
-    init(row: Int, column col: Int, withRepeatedValue value: T) {
+    init(row:Int, column col:Int, withRepeatedValue value: T)
+    {
         rowCapacity = row
         colCapacity = col
         
-        matrix = ObjcLiteMatrix(rowSize: row, withColumnSize: col)
+        matrix = ObjcLiteMatrix(rowSize:row, withColumnSize:col)
         warning = "Index out of range - row capacity: \(rowCapacity), col capacity: \(colCapacity)"
         
         for i in 0..<rowCapacity {
@@ -53,38 +55,50 @@ class LiteMatrix<T: NSObject> {
     
     
     // Double subscript notation is the only public interface: exampleMatrix[1,2]
-    subscript(row: Int, col: Int) -> T {
+    subscript(row:Int, col:Int) -> T
+        {
         get {
+            //index = (x:row, y:col)
+            //updateWarning()
             assert(indexIsValidForRow(row, column: col), warning)
             return getObjectFromMatrixAtRow(row, column: col)
         }
+        
         set {
+            //index = (row, col)
+            //updateWarning()
             assert(indexIsValidForRow(row, column: col),warning)
             setObjectInMatrixAtIndex(newValue, row: row, column: col)
         }
     }
-
-
-    private func getObjectFromMatrixAtRow(row:Int, column col: Int) -> T {
+    
+    
+    private func getObjectFromMatrixAtRow(row:Int, column col:Int) -> T
+    {
         let nilWarning = "Nil returned from matrix at index \(row), \(col)"
+        
         let obj = matrix.accessObjectAtRow(row, column: col) as? T
         assert(obj != nil, nilWarning)
         return obj!
     }
-
-
-    private func setObjectInMatrixAtIndex(object: T, row: Int, column col: Int) {
+    
+    
+    private func setObjectInMatrixAtIndex(object:T, row:Int, column col:Int)
+    {
         matrix.addObjectToMatrixAtIndex(object as AnyObject, row: row, column: col)
     }
-
-
+    
+    
     /* Debugging */
-    private func updateWarning() {
+    private func updateWarning()
+    {
         warning = "Index out of range - \(index) - row capacity: \(rowCapacity), col capacity: \(colCapacity)"
     }
-
+    
+    
     /* Debugging */
-    private func indexIsValidForRow(row: Int, column col: Int) -> Bool {
+    private func indexIsValidForRow(row:Int, column col:Int) -> Bool
+    {
         return (row >= 0 && row < rowCapacity) && (col >= 0 && col < colCapacity)
     }
 }
@@ -103,9 +117,11 @@ extension LiteMatrix : CollectionType {
         return rowCapacity - 1
     }
     
-    // Generator for implementing for..in iteration
-    func generate() -> AnyGenerator<T> {
     
+    // Generator for implementing for..in iteration
+    func generate() -> AnyGenerator<T>
+    {
+        
         let rowMax = rowCapacity
         let colMax = colCapacity
         
@@ -113,13 +129,17 @@ extension LiteMatrix : CollectionType {
         var colIndex = 0
         
         // Passes function definition for next() on init with a closure
-        return anyGenerator {
+        return AnyGenerator {
             if rowIndex < rowMax {
                 if colIndex < colMax {
-                    return self.getObjectFromMatrixAtRow(rowIndex, column: colIndex++)
+                    let col = colIndex
+                    colIndex += 1
+                    return self.getObjectFromMatrixAtRow(rowIndex, column: col)
                 } else {
                     colIndex = 0
-                    return self.getObjectFromMatrixAtRow(rowIndex++, column: colIndex)
+                    let row = rowIndex
+                    rowIndex += 1
+                    return self.getObjectFromMatrixAtRow(row, column: colIndex)
                 }
             } else {
                 return nil
@@ -127,12 +147,14 @@ extension LiteMatrix : CollectionType {
         }
     }
     
-    // A single subscript, i, will return what is in the column at index i, in the first row
+    
+    // A single subscript, i, will return what is in the column at index 0, in the first row
     // This is only implemented to conform to the CollectionType protocol and the objects
-    // in the matrix ought not be accessed this way. This is prone to index out of bounds.
-    internal subscript(i: Int) -> T {
-        assert(indexIsValidForRow(0, column: i), warning)
-        return getObjectFromMatrixAtRow(0, column: i)
+    // in the matrix ought not be accessed this way.
+    internal subscript(i:Int) -> T
+    {
+        assert(indexIsValidForRow(0, column: 0), warning)
+        return getObjectFromMatrixAtRow(0, column: 0)
     }
     
 } // End of extension CollectionType
